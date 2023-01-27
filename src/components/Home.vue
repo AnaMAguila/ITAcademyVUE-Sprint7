2918989c16aac1418cb24000600faa1a56bbf763
Ana María Águila <ana_mari_aguila@hotmail.com>
2023-01-27 11:30:40 +0100

    Ej2. Cantidad páginas e idiomas

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,55 +2,83 @@
     <div class="form">
         <p>¿Què vols fer?</p>
         <p v-for="servicio in servicios" :key="servicio">
-          <input type="checkbox" :id="servicio.title" v-model="servicio.checked" @click="isChecked(servicio)">
-          {{ servicio.title }}
+            <input type="checkbox" :id="servicio.title" v-model="servicio.checked">
+            {{ servicio.title }}
+
+        <div v-if="servicio.pages != null && servicio.checked == true">
+            <Panell :servicio=servicio></Panell>
+        </div>
         </p>
-        Preu: {{ total }}€   
+        Preu: {{ newTotal }}€
     </div>
 </template>
 
 <script>
+import Panell from './Panell.vue'
+
 export default {
-  name: 'Home',
-  data() {
-    return {
-      servicios: [
-        {
-          title: "Obtenir el pressupost d'una pàgina web (500 €)",
-          price: 500,
-          checked: false
-        },
-        {
-          title: "Fer una campanya SEO (300 €)",
-          price: 300,
-          checked: false
-        },
-        {
-          title: "Fer una campanya de publicitat (200 €)",
-          price: 200,
-          checked: false
+    name: 'Home',
+    components: {
+        Panell
+    },
+    data() {
+        return {
+            servicios: [
+                {
+                    title: "Obtenir el pressupost d'una pàgina web (500 €)",
+                    price: 500,
+                    checked: false,
+                    pages: 0,
+                    lang: 0
+                },
+                {
+                    title: "Fer una campanya SEO (300 €)",
+                    price: 300,
+                    checked: false
+                },
+                {
+                    title: "Fer una campanya de publicitat (200 €)",
+                    price: 200,
+                    checked: false
+                }
+            ],
+            total: 0
         }
-      ],
-      total: 0
+    },
+    methods: {
+        // Si el servicio está seleccionado suma en el total, en caso contrario, resta
+        // isChecked(servicio) {
+        //   servicio.checked ? this.total -= servicio.price : this.total += servicio.price
+        // }
+    },
+    computed: {
+        // calcula el presupuesto total con páginas e idiomas incluidos
+        newTotal() {
+            let total = 0;
+
+            this.servicios.map(function (servicio) {
+                if (servicio.checked) {
+                    total += servicio.price;
+
+                    if (servicio.pages > 0 && servicio.lang > 0) {
+                        total += (servicio.pages * servicio.lang * 30)
+                    }
+                }
+            })
+            return total;
+        }
     }
-  },
-  methods: {
-    // Si el servicio está seleccionado suma en el total, en caso contrario, resta
-    isChecked(servicio) {
-      servicio.checked ? this.total -= servicio.price : this.total += servicio.price
-    }
-  }
 }
 </script>
 
 <style>
-  * {
+* {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-  }
+}
 
-  .form {
+.form {
     margin: 10px;
-  }
+}
 </style>
---
+++ src/components/Panell.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="opc-web">
+        <table>
+            <tr>
+                <!-- v-model envia el valor del input -->
+                <td>Número de pàgines</td>
+                <td><input type="text" v-model.number="pages"></td>
+            </tr>
+            <tr>
+                <td>Número d'idiomes</td>
+                <td><input type="text" v-model.number="lang"></td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Panell',
+        props: ['servicio'],
+        data (){
+            return {
+                pages: this.servicio.pages,
+                lang: this.servicio.lang
+            }
+        },        
+        watch: { // cuando cambia el valor de los inputs se envía al array de servicios
+            pages: function(value){
+                this.servicio.pages=value
+            },
+            lang: function(value){
+                this.servicio.lang=value
+            }
+        }
+    }
+</script>
+
+<style>
+    .opc-web {
+        margin-top: 20px;
+        width: fit-content;
+        border: 4px solid #000;
+        padding: 20px 10px 20px 10px;
+        border-radius: 20px;
+    }
+
+    td {
+        padding: 10px;
+    }
+</style>
